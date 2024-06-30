707f30409def8465dce3916e419c8debcca24b29
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-06-30 22:17:26 +0700

    Update hero styling

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,10 +1,14 @@
 export default function HeroSection() {
   return (
-    <section className="bg-hero-image bg-cover bg-center text-zinc-50 w-full h-96 flex flex-col justify-center items-end border-b-2 border-zinc-950 p-12">
-      <h1 className="text-7xl sm:text-8xl font-bold text-center sm:text-right bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-purple-500">
-        SEA Salon
-      </h1>
-      <p className="text-xl text-purple-600 text-center sm:text-right">Beauty and Elegance Redefined</p>
+    <section className="bg-hero-image bg-cover bg-center w-full h-96 border-b-2 border-pink-500">
+      <div className="backdrop-blur-sm w-full h-full flex flex-col py-36 px-12">
+        <h1 className="text-7xl sm:text-8xl font-bold text-center sm:text-right bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+          SEA Salon
+        </h1>
+        <p className="text-xl font-bold text-center sm:text-right">
+          Beauty and Elegance Redefined
+        </p>
+      </div>
     </section>
   );
 }
