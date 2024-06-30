a6b00c1479eff7a29209aa56900be446102ba74a
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-06-30 22:33:28 +0700

    feat: Change navbar

--- components/header.tsx
+++ components/header.tsx
@@ -3,15 +3,13 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="bg-zinc-950/80 text-zinc-50 flex items-center justify-between px-6 py-4 fixed w-full top-0 z-10 backdrop-blur-sm border-b-zinc-300 border-b-2">
+    <header className="bg-zinc-950/70 text-zinc-50 flex items-center justify-between px-8 py-4 fixed w-[95%] mt-4 rounded-full top-0 z-10 backdrop-blur-sm border-zinc-300 border-2">
       <Link
         href="/"
         className="flex gap-4 items-center"
       >
         <ScissorsIcon className="w-6 h-6" />
-        <h1 className="font-bold text-2xl">
-          SEA Salon
-        </h1>
+        <h1 className="font-bold text-2xl">SEA Salon</h1>
       </Link>
       <nav className="hidden md:block">
         <ul className="flex space-x-6">
